bbc55bafd3c5ac4c2a68028f6763cef3ffa0f383
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-06-08 10:05:06 -0500

    se integran nuevas dependencias para que funcionen los cors

--- BeMindBackend/src/index.ts
+++ BeMindBackend/src/index.ts
@@ -1,12 +1,22 @@
-import express from 'express';
-import TaskRoute from './routes/TaskRoute';
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookiesParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import TaskRoute from "./routes/TaskRoute";
 
 const app = express();
 const _PORT = 3000;
 
-app.use(express.json());
-app.use('/api', TaskRoute);
+app.use(cors({ credentials: true }));
+app.use(compression());
+app.use(cookiesParser());
+app.use(bodyParser.json());
+app.use("/api", TaskRoute);
 
-app.listen(_PORT, () => {
-    console.log(`Server running on port: http://localhost:${_PORT}/api`);
+const server = http.createServer(app);
+
+server.listen(_PORT, () => {
+  console.log(`Server running on http://localhost:${_PORT}/api`);
 });
